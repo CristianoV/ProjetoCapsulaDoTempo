b6412e19798b74fe2b1f2c22af3695b9733f43d6
CristianoV <cristianoviieira@gmail.com>
2024-12-03 01:45:43 -0300

    altera o dominio

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'https://projeto-capsula-do-tempo-pearl.vercel.app'],
+    domains: ['avatars.githubusercontent.com', 'projeto-capsula-do-tempo-pearl.vercel.app'],
   },
-}
+};
 
 export default nextConfig;
