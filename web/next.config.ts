327b29eb8859f825e05316138140401c0c33fd4b
CristianoV <cristianoviieira@gmail.com>
2024-12-03 01:08:18 -0300

    adiciona link para renderizacao de imagem

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'localhost'],
+    domains: ['avatars.githubusercontent.com', 'https://projeto-capsula-do-tempo-pearl.vercel.app'],
   },
 }
 
